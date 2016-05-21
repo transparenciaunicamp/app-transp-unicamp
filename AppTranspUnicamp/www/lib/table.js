cd02e6190f8b11f83c23db9d5eb8f9f761ef46ad
cmillani <carloseduardomillani@gmail.com>
2016-05-21 17:43:03 -0300

    Adjusting timeline width and adding some color to posts

--- AppTranspUnicamp/www/lib/table.js
+++ AppTranspUnicamp/www/lib/table.js
@@ -1,14 +1,25 @@
+var currentClass = 'one';
+var nextClass = 'anotherone';
+
 var FeedTable = {
     addRow: function(message) {
-        var table = document.getElementById("myTable");
-        var row = table.insertRow(0);
-        var cell1 = row.insertCell(0);
-        cell1.innerHTML = message;
-        return cell1;
+//        var table = document.getElementById("myTable");
+//        var row = table.insertRow(0);
+//        var cell1 = row.insertCell(0);
+//        cell1.innerHTML = message;
+//        return cell1;
+        
+        var coisinha = $('<p align="justify" class="'+currentClass+'">'+message+'</p>');
+        coisinha.appendTo('#myTable');
+        
+        var tempClass = currentClass;
+        currentClass = nextClass;
+        nextClass = tempClass;
+        
+        return coisinha;
     },
     insertImage: function(cell, link) {
-        var img = document.createElement('img');
-        img.src = link;
-        cell.appendChild(img);
+        var img = $('<img src="'+link+'" width="100%">');
+        cell.append(img);
     }
 }
