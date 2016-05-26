7fd2d6229c896c1c984be5c5f13b6e828de8933f
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2016-05-26 11:23:49 -0300

    speed of change of pages increased

--- Nos/Nos/www/js/map.js
+++ Nos/Nos/www/js/map.js
@@ -1,13 +1,3 @@
-var mapShown = false;
-        
-function showMap() {
-    if(!mapShown) {
-        var map = $("<div class='map'><iframe id='mapFrame' onload='ifFrameLoaded()' height='100%' width='100%' frameBorder='0' src='https://a.tiles.mapbox.com/v4/transparenciaunicamp.057nf8mf/attribution,zoompan,zoomwheel.html?access_token=pk.eyJ1IjoidHJhbnNwYXJlbmNpYXVuaWNhbXAiLCJhIjoiY2lvYWYzdzhsMDNpN3cxa3F0M2czaGtraiJ9.Snu_l3HM0COOW0V39I0LvA'></iframe></div>")
-        map.appendTo('#myMap')
-        mapShown = true
-    }
-}
-
 function ifFrameLoaded() {
     $('#mapFrame').css('height', $(window).height()+'px');
 }
