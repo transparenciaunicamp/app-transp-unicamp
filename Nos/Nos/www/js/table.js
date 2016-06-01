8c76329b42db8a59fa5f24d480a50aa3cf64d27a
cmillani <carloseduardomillani@gmail.com>
2016-06-01 12:37:20 -0300

    Small fix to time ago in post

--- Nos/Nos/www/js/table.js
+++ Nos/Nos/www/js/table.js
@@ -5,53 +5,17 @@ var FeedTable = {
         
         Facebook.getName(page, function (name) {
             Facebook.getProfilePicture(page, function(picUrl) {
-                var newItem = ['<div class="','"><div><div style="width: 100%; overflow: hidden;"><div style="width: 50px; float: left;"> <img src="'+picUrl+'"> </div><div style="margin-left: 60px;"><h2>'+name+'</h2><br><i>'+timeSince(new Date(date))+'</i></div></div><br><p>'+message+'</p></div><img src="'+imageSrc+'" width="100%"></div>']; //Separates in an array so i can add class later, after the full list is sorted
+                var newItem = ['<div class="','"><div><div style="width: 100%; overflow: hidden;"><div style="width: 50px; float: left;"> <img src="'+picUrl+'"> </div><div style="margin-left: 60px;"><h2>'+name+'</h2><br><i>'+new Date(date).timeSince()+'</i></div></div><br><p>'+message+'</p></div><img src="'+imageSrc+'" width="100%"></div>']; //Separates in an array so i can add class later, after the full list is sorted
                 done(newItem, date);
             })
         })
     }
 }
 
-Date.prototype.ddmmyyy = function() {
-   var yyyy = this.getFullYear().toString();
-   var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-   var dd  = this.getDate().toString();
-   return (dd[1]?dd:"0"+dd[0]) + "/" + (mm[1]?mm:"0"+mm[0]) + "/" + yyyy; // padding
-  };
-
-function timeSince(date) {
-
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
-}
-
 Date.prototype.timeSince = function() {
-    console.log(this);
-    console.log((new Date() - this)/1000)
+
     var seconds = Math.floor((new Date() - this) / 1000);
-    console.log("SECONDS::::" + seconds);
+
     var interval = Math.floor(seconds / 31536000);
 
     if (interval > 1) {
